1598590b07135b6f9c03d35f39ac31ed832a3b3d
kasha <kasha1147@gmail.com>
2021-01-07 08:43:03 +0300

    comment and delete some code to test changes

--- src/main/java/com/savushkinvyacheslav/CalculatorOfProbabilityBot.java
+++ src/main/java/com/savushkinvyacheslav/CalculatorOfProbabilityBot.java
@@ -1,41 +1,38 @@
 package com.savushkinvyacheslav;
 
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import com.savushkinvyacheslav.commands.HelpCommand;
+import com.savushkinvyacheslav.commands.StartCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.text.DecimalFormat;
-import java.util.regex.*;
 
-public class CalculatorOfProbabilityBot extends TelegramLongPollingBot {
-
-    private static final String MANUAL = "In order for the bot to calculate, " +
-            "you need to send it a message in the following format:" +
-            "\n\ndicesYouRoll+\\[-]modifier>=difficultyOfCheck" +
-            "\n\nWhere:" +
-            "\ndicesYouRoll - set of dices you roll. Each dice has the form like this: dx, where x - number of dice faces. " +
-            "Bot supports dices with following faces: 4, 6, 8, 10, 12, 20, 100. All dices listed separated by plus (+) symbol. " +
-            "If your dice set contains 2+ identical dices you may unite it by typing that: ydx, where y - amount of identical dices. " +
-            "\nmodifier - Integer number that you need to add to your check. If it's positive - type \"+\" before modifier, " +
-            "if it's negative - type \"-\" before modifier, if it's 0 you may not type it at all." +
-            "\ndifficultyOfCheck - natural number that determines the difficulty of the check.";
-
-    private static final String START_MESSAGE = "Hello, this bot calculates the probability of a successful dice roll check" +
-            "\nTo succeed at the check, the result of your dice roll and modifiers " +
-            "must be greater than or equal to the difficulty of the check.\n" + MANUAL;
-
-    private static final String HELP_MESSAGE = MANUAL +
-            "\nSome examples of right message format:" +
-            "\nd4>=2" +
-            "\nd4+d12>=15" +
-            "\n4d4+4d10>=45";
+public class CalculatorOfProbabilityBot extends TelegramLongPollingCommandBot {
 
     private static final String INCORRECT_DATA_MESSAGE = "Incorrect message entered" +
             "\nType /help for more information";
 
+    public CalculatorOfProbabilityBot() {
+        super();
+        register(new StartCommand("start", "Start"));
+        register(new HelpCommand("help", "Help"));
+    }
+
     @Override
+    public void processNonCommandUpdate(Update update) {
+        SendMessage answer = new SendMessage();
+        answer.setText(update.getMessage().getText());
+        answer.setChatId(update.getMessage().getChatId().toString());
+        try {
+            execute(answer);
+        } catch (TelegramApiException e) {
+            System.out.println("gigi");
+        }
+    }
+
+    /*@Override
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
 
@@ -68,9 +65,9 @@ public class CalculatorOfProbabilityBot extends TelegramLongPollingBot {
             else
                 sendMsg(chatId, INCORRECT_DATA_MESSAGE);
         }
-    }
+    }*/
 
-    private void sendMsg(String chatId, String answer) {
+    /*private void sendMsg(String chatId, String answer) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(chatId);
@@ -80,7 +77,7 @@ public class CalculatorOfProbabilityBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
     public static String parseAndCount(String expression) {
         String[] partsOfExpression = expression.split(">=");
