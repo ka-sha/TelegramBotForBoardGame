c0ed0a3afa563f61fa6f81353f3d6f2ebf93d368
kasha <kasha1147@gmail.com>
2021-01-07 17:26:03 +0300

    add call for ParserOfCheckExpression

--- src/main/java/com/savushkinvyacheslav/AnswerFormatter.java
+++ src/main/java/com/savushkinvyacheslav/AnswerFormatter.java
@@ -2,13 +2,17 @@ package com.savushkinvyacheslav;
 
 public class AnswerFormatter {
 
-    private static final String INCORRECT_DATA_MESSAGE = "Incorrect message entered" +
-            "\nType /help for more information";
+    private static final String INCORRECT_DATA_MESSAGE = "Incorrect message entered\n" +
+            "Type /help for more information";
 
     public static String getAnswer(String messageText) {
-        if (ParserOfCheckExpression.isValidForCalculating(messageText)) {
+        if (ParserOfCheckExpression.isValidForCalculateProbability(messageText)) {
+            CheckExpression probabilityCheckExpression =
+                    ParserOfCheckExpression.getCheckExpressionForCalculateProbability(messageText);
+        } else if (ParserOfCheckExpression.isValidForCompareExpressions(messageText)) {
             return "some string";
-        } else
-            return INCORRECT_DATA_MESSAGE;
+        }
+
+        return INCORRECT_DATA_MESSAGE;
     }
 }
