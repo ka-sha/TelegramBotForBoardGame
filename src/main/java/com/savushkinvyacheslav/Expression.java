69759910940716ea197820bccc66485a502b5d56
kasha <kasha1147@gmail.com>
2021-01-08 07:43:38 +0300

    made all method non static

--- src/main/java/com/savushkinvyacheslav/Expression.java
+++ src/main/java/com/savushkinvyacheslav/Expression.java
@@ -2,65 +2,78 @@ package com.savushkinvyacheslav;
 
 import java.util.regex.*;
 
-public class ParserOfCheckExpression {
+public class Expression {
 
+    private final String expression;
     private static final String diceSetRegex = "(((([1-9]*)[d])([468]|10|12|20|100)([+]?))+([+]\\\\d+)?)";
 
-    public static boolean isValidForCalculateProbability(String expression) {
+    public Expression(String expression) {
+        this.expression = expression;
+    }
+
+    public boolean isValidForCalculateProbability() {
         Pattern calculateProbabilityRegex = Pattern.compile("^" + diceSetRegex + "(>=\\\\d+)$");
         Matcher matcher = calculateProbabilityRegex.matcher(expression);
-        return matcher.find() && noContainPlusOnEnd(expression);
+        return matcher.find() && noContainPlusOnLeftPartEnd();
     }
 
-    public static boolean isValidForCompareExpressions(String expression) {
+    public boolean isValidForCompare() {
         Pattern compareExpressionRegex = Pattern.compile("^" + diceSetRegex + "\\p{Blank}" + diceSetRegex + "$");
         Matcher matcher = compareExpressionRegex.matcher(expression);
-        return matcher.find() && noContainPlusOnEnd(expression);
+        return matcher.find() && noContainPlusOnLeftPartEnd();
     }
 
-    private static boolean noContainPlusOnEnd(String expression) {
+    private boolean noContainPlusOnLeftPartEnd() {
         return !expression.contains("+>");
     }
 
-    public static CheckExpression getCheckExpressionForCalculateProbability(String expression) {
+    public CheckExpression[] getCheckExpressionsForComparing() {
+        String[] strExpressions = expression.split("\\p{Blank}");
+        Expression e1 = new Expression(strExpressions[0]);
+        Expression e2 = new Expression(strExpressions[1]);
+        return new CheckExpression[]{e1.getCheckExpressionForCalculateProbability(),
+                e2.getCheckExpressionForCalculateProbability()};
+    }
+
+    public CheckExpression getCheckExpressionForCalculateProbability() {
         DiceSet diceSet;
         int modifier;
-        int difficultyOfCheck = parseDifficultyOfCheck(expression);
-        if (hasModifier(expression)) {
-            diceSet = parseDiceSet(expression, "+");
-            modifier = parseModifier(expression);
+        int difficultyOfCheck = parseDifficultyOfCheck();
+        if (hasModifier()) {
+            diceSet = parseDiceSet("+");
+            modifier = parseModifier();
         } else {
-            diceSet = parseDiceSet(expression, ">");
+            diceSet = parseDiceSet(">");
             modifier = 0;
         }
 
         return new CheckExpression(diceSet, modifier, difficultyOfCheck);
     }
 
-    private static int parseDifficultyOfCheck(String expression) {
+    private int parseDifficultyOfCheck() {
         int startOfDigit = expression.indexOf("=") + 1;
         String strDigit = expression.substring(startOfDigit);
         return Integer.parseInt(strDigit);
     }
 
-    private static boolean hasModifier(String expression) {
-        return !lastTerm(expression).contains("d");
+    private boolean hasModifier() {
+        return !lastTerm().contains("d");
     }
 
-    private static String lastTerm(String expression) {
+    private String lastTerm() {
         int lastPlus = expression.lastIndexOf("+");
         int aboveSymbolIndex = expression.indexOf(">");
         return expression.substring(lastPlus + 1, aboveSymbolIndex);
     }
 
-    private static DiceSet parseDiceSet(String expression, String lastSymbol) {
-        int lastPlus = expression.lastIndexOf(lastSymbol);
-        String strDiceSet = expression.substring(0, lastPlus);
+    private DiceSet parseDiceSet(String lastSymbol) {
+        int lastSymbolIndex = expression.lastIndexOf(lastSymbol);
+        String strDiceSet = expression.substring(0, lastSymbolIndex);
         return new DiceSet(strDiceSet.split("[+]"));
     }
 
-    private static int parseModifier(String expression) {
-        String modifier = lastTerm(expression);
+    private int parseModifier() {
+        String modifier = lastTerm();
         return Integer.parseInt(modifier);
     }
 }
