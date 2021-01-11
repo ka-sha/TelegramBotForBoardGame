44ff7e7dddc7aada8166fe55b9b7eaf7333de93f
kasha <kasha1147@gmail.com>
2021-01-11 17:35:00 +0300

    rewrite answers

--- src/main/java/com/savushkinvyacheslav/commands/HelpCommand.java
+++ src/main/java/com/savushkinvyacheslav/commands/HelpCommand.java
@@ -4,22 +4,19 @@ import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 public class HelpCommand extends ServiceCommand {
-
-    //TODO: rewrite ANSWER
-    private static final String ANSWER = "In order for the bot to calculate, " +
-            "you need to send it a message in the following format:" +
-            "\n\ndicesYouRoll+\\[-]modifier>=difficultyOfCheck" +
-            "\n\nWhere:" +
-            "\ndicesYouRoll - set of dices you roll. Each dice has the form like this: dx, where x - number of dice faces. " +
+    private static final String ANSWER = BOT_SKILLS + "\n\n" +
+            "Syntax for first type of command (to calculate probability of a successful dice roll check):\n\n" +
+            "dicesYouRoll[+modifier]>=difficultyOfCheck\n" +
+            "Where:\n" +
+            "dicesYouRoll - set of dices you roll. Each dice has the form like this: dx, where x - number of dice faces. " +
             "Bot supports dices with following faces: 4, 6, 8, 10, 12, 20, 100. All dices listed separated by plus (+) symbol. " +
-            "If your dice set contains 2+ identical dices you may unite it by typing that: ydx, where y - amount of identical dices. " +
-            "\nmodifier - Integer number that you need to add to your check. If it's positive - type \"+\" before modifier, " +
-            "if it's negative - type \"-\" before modifier, if it's 0 you may not type it at all." +
-            "\ndifficultyOfCheck - natural number that determines the difficulty of the check." +
-            "\nSome examples of right message format:" +
-            "\nd4>=2" +
-            "\nd4+d12>=15" +
-            "\n4d4+4d10>=45";
+            "If your dice set contains 2+ identical dices you may unite it by typing that: ydx, where y - amount of identical dices.\n" +
+            "modifier - natural number that you need to add to your check. If it's 0 you may not type it.\n" +
+            "difficultyOfCheck - natural number that determines the difficulty of the check.\n\n" +
+            "Syntax for second type of command (to compare 2 dice sets):\n\n" +
+            "dicesYouRoll[+modifier] dicesYouRoll[+modifier]\n" +
+            "diceYouRoll - see above\n" +
+            "modifier - see above\n";
 
     public HelpCommand(String commandIdentifier, String description) {
         super(commandIdentifier, description);
--- src/main/java/com/savushkinvyacheslav/commands/StartCommand.java
+++ src/main/java/com/savushkinvyacheslav/commands/StartCommand.java
@@ -4,21 +4,10 @@ import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 public class StartCommand extends ServiceCommand {
-
-    //TODO: rewrite ANSWER
-    private static final String ANSWER = "Hello, this bot calculates the probability of a successful dice roll check" +
-            "\nTo succeed at the check, the result of your dice roll and modifiers " +
-            "must be greater than or equal to the difficulty of the check.\n" +
-            "In order for the bot to calculate, " +
-            "you need to send it a message in the following format:" +
-            "\n\ndicesYouRoll+\\[-]modifier>=difficultyOfCheck" +
-            "\n\nWhere:" +
-            "\ndicesYouRoll - set of dices you roll. Each dice has the form like this: dx, where x - number of dice faces. " +
-            "Bot supports dices with following faces: 4, 6, 8, 10, 12, 20, 100. All dices listed separated by plus (+) symbol. " +
-            "If your dice set contains 2+ identical dices you may unite it by typing that: ydx, where y - amount of identical dices. " +
-            "\nmodifier - Integer number that you need to add to your check. If it's positive - type \"+\" before modifier, " +
-            "if it's negative - type \"-\" before modifier, if it's 0 you may not type it at all." +
-            "\ndifficultyOfCheck - natural number that determines the difficulty of the check.";
+    private static final String ANSWER = BOT_SKILLS + "\n" +
+            "Example for 1: d12+3d8+10>=16\n" +
+            "Example for 2: d10+d4+2 d12+d6+3\n\n" +
+            "For more information type /help";
 
     public StartCommand(String commandIdentifier, String description) {
         super(commandIdentifier, description);
