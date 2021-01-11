c6859193305276d544d8c1ffc52d196302494ded
kasha <kasha1147@gmail.com>
2021-01-11 13:54:57 +0300

    fix makeComparingTable method

--- src/main/java/com/savushkinvyacheslav/AnswerFormatter.java
+++ src/main/java/com/savushkinvyacheslav/AnswerFormatter.java
@@ -4,6 +4,7 @@ public class AnswerFormatter {
 
     private static final String INCORRECT_DATA_MESSAGE = "Incorrect message entered\n" +
             "Type /help for more information";
+    private static final String PERCENT_SYMBOL = " %";
 
     public static String getAnswer(String messageText) {
         Expression expression = new Expression(messageText);
@@ -11,7 +12,7 @@ public class AnswerFormatter {
             CheckExpression probabilityCheckExpression =
                     expression.getCheckExpressionForCalculateProbability();
             Double probability = probabilityCheckExpression.calculateProbability();
-            return formatDouble(probability);
+            return formatDouble(probability) + PERCENT_SYMBOL;
         } else if (expression.isValidForCompare()) {
             CheckExpression[] expressions = expression.getCheckExpressionsForComparing();
             return makeComparingTable(expressions);
@@ -25,25 +26,34 @@ public class AnswerFormatter {
     }
 
     private static String makeComparingTable(CheckExpression[] expressionPair) {
-        CheckExpression ex1 = expressionPair[0];
-        CheckExpression ex2 = expressionPair[1];
+        CheckExpression ce1 = expressionPair[0];
+        CheckExpression ce2 = expressionPair[1];
         StringBuilder table = new StringBuilder(
-                String.format("| %-3s | %.8s, %% | %.8s, %% |\n", ">=", ex1, ex2));
-        int n = Math.max(ex1.getDiceSet().maxSumDiceValues(), ex2.getDiceSet().maxSumDiceValues());
+                String.format("| %-3s | %.8s, %% | %.8s, %% |\n",
+                        ">=",
+                        formatDiceSetPlusModifier(ce1),
+                        formatDiceSetPlusModifier(ce2)));
+        int n = Math.max(ce1.getDiceSet().maxSumDiceValues(), ce2.getDiceSet().maxSumDiceValues());
         int difficulty = 1;
         while (difficulty <= n) {
-            double probability1 = ex1.calculateProbability();
-            double probability2 = ex2.calculateProbability();
+            double probability1 = ce1.calculateProbability();
+            double probability2 = ce2.calculateProbability();
             if (notEqualToHundred(probability1, probability2)) {
                 String pos1 = formatDouble(probability1);
                 String pos2 = formatDouble(probability2);
                 table.append(String.format("| %-3s | %11s | %11s |\n", "" + difficulty, pos1, pos2));
             }
             difficulty++;
-            ex1.setDifficultyOfCheck(difficulty);
-            ex2.setDifficultyOfCheck(difficulty);
+            ce1.setDifficultyOfCheck(difficulty);
+            ce2.setDifficultyOfCheck(difficulty);
         }
         return table.toString();
+    }
+
+    private static String formatDiceSetPlusModifier(CheckExpression ce) {
+        int modifier = ce.getModifier();
+        DiceSet ds = ce.getDiceSet();
+        return (modifier == 0) ? ds.toString() : ds.toString() + "+" + modifier;
     }
 
     private static boolean notEqualToHundred(double x, double y) {
