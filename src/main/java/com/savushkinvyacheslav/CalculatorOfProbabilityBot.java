2f8fe5fd4732226e818adda9d8953acc77b2b0dd
kasha <kasha1147@gmail.com>
2021-01-11 13:18:50 +0300

    delete unused methods

--- src/main/java/com/savushkinvyacheslav/CalculatorOfProbabilityBot.java
+++ src/main/java/com/savushkinvyacheslav/CalculatorOfProbabilityBot.java
@@ -6,8 +6,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.text.DecimalFormat;
-
 public class CalculatorOfProbabilityBot extends TelegramLongPollingCommandBot {
 
     public CalculatorOfProbabilityBot() {
@@ -20,7 +18,7 @@ public class CalculatorOfProbabilityBot extends TelegramLongPollingCommandBot {
     public void processNonCommandUpdate(Update update) {
         Message message = update.getMessage();
 
-        if(isValidMessageAnswer(message)) {
+        if (isValidMessageAnswer(message)) {
             Long chatId = message.getChatId();
             String text = message.getText();
             String answer = AnswerFormatter.getAnswer(text);
@@ -41,68 +39,6 @@ public class CalculatorOfProbabilityBot extends TelegramLongPollingCommandBot {
         } catch (TelegramApiException e) {
             System.out.println("future log");
         }
-    }
-
-    /*String diceSetRegex = "(((([1-9]*)[d])([468]|10|12|20|100)([+]?))+([+]\\d+)?)";
-      Pattern CalculateProbabilityPattern = Pattern.compile("^" + diceSetRegex + "(>=\\d+)$");
-      Pattern CompareTwoDiceSetsPattern = Pattern.compile("^" + diceSetRegex + "\\p{Blank}" + diceSetRegex + "$");
-      Matcher matcher = CalculateProbabilityPattern.matcher(text);*/
-
-    public static String parseAndCount(String expression) {
-        String[] partsOfExpression = expression.split(">=");
-        int boarder = Integer.parseInt(partsOfExpression[1]);
-        int modifier = parseModifier(partsOfExpression[0]);
-        String preDiceSet = prepareStringDiceSet(partsOfExpression[0], modifier == 0);
-
-        DiceSet diceSet = new DiceSet(preDiceSet.split("[+]"));
-
-        double result = diceSet.probabilityInPercents(diceSet.aboveOrEquals(boarder - modifier));
-        return new DecimalFormat("#.##").format(result);
-    }
-
-    private String makeComparingTable(String expression) {
-        String[] expressions = expression.split("\\p{Blank}");
-        if (expressions.length != 2)
-            return "need to delete this";
-
-        return createComparingTable(expressions[0], expressions[1]);
-    }
-
-    private static int parseModifier(String expression) {
-        String lastTerm = expression.substring(expression.lastIndexOf("+") + 1);
-        if (lastTerm.contains("d"))
-            return 0;
-        else
-            return Integer.parseInt(lastTerm);
-    }
-
-    private static String prepareStringDiceSet(String expression, boolean zeroModifier) {
-        return zeroModifier ? expression :
-                expression.substring(0, expression.lastIndexOf("+"));
-    }
-
-    private String createComparingTable(String expression1, String expression2) {
-        int modifier1 = parseModifier(expression1);
-        int modifier2 = parseModifier(expression2);
-        String stringDiceSet1 = prepareStringDiceSet(expression1, modifier1 == 0);
-        String stringDiceSet2 = prepareStringDiceSet(expression2, modifier2 == 0);
-        DiceSet ds1 = new DiceSet(stringDiceSet1.split("[+]"));
-        DiceSet ds2 = new DiceSet(stringDiceSet2.split("[+]"));
-
-        StringBuilder resultTable = new StringBuilder(String.format("| %-3s | %.8s, %% | %.8s, %% |\n", ">=", expression1, expression2));
-        int difficultyOfCheck = Math.min(ds1.minSumDiceValues() + modifier1, ds2.minSumDiceValues() + modifier2);
-        int n = Math.max(ds1.maxSumDiceValues(), ds2.maxSumDiceValues());
-
-        while (difficultyOfCheck <= n) {
-            double probabilityOfSuccess1 = ds1.probabilityInPercents(ds1.aboveOrEquals(difficultyOfCheck - modifier1));
-            double probabilityOfSuccess2 = ds2.probabilityInPercents(ds2.aboveOrEquals(difficultyOfCheck - modifier2));
-            String pos1 = String.format("%.2f", probabilityOfSuccess1);
-            String pos2 = String.format("%.2f", probabilityOfSuccess2);
-            resultTable.append(String.format("| %-3s | %11s | %11s |\n", "" + difficultyOfCheck, pos1, pos2));
-            difficultyOfCheck++;
-        }
-
-        return resultTable.toString();
     }
 
     @Override
