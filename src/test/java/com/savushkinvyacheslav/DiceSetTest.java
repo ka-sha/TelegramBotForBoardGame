39ce13139050cdb56e242b1ba760fe985d586e21
kasha <kasha1147@gmail.com>
2021-01-11 17:59:31 +0300

    rewrite some tests

--- src/test/java/com/savushkinvyacheslav/DiceSetTest.java
+++ src/test/java/com/savushkinvyacheslav/DiceSetTest.java
@@ -44,31 +44,31 @@ public class DiceSetTest {
 
     @Test
     public void toStringVariousSeveralDicesTest() {
-        assertEquals("d4+d6", new DiceSet(new String[]{"d4", "d6"}).toString());
-        assertEquals("d4+d6+d8", new DiceSet(new String[]{"d4", "d6", "d8"}).toString());
-        assertEquals("d4+d6+d8+d10", new DiceSet(new String[]{"d4", "d6", "d8", "d10"}).toString());
-        assertEquals("d4+d6+d8+d10+d12", new DiceSet(new String[]{"d4", "d6", "d8", "d10", "d12"}).toString());
+        assertEquals("d6+d4", new DiceSet(new String[]{"d4", "d6"}).toString());
+        assertEquals("d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "d8"}).toString());
+        assertEquals("d10+d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "d8", "d10"}).toString());
+        assertEquals("d12+d10+d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "d8", "d10", "d12"}).toString());
 
-        assertEquals("4d4+d6+d8+d10+d12", new DiceSet(new String[]{"4d4", "d6", "d8", "d10", "d12"}).toString());
-        assertEquals("d4+4d6+d8+d10+d12", new DiceSet(new String[]{"d4", "4d6", "d8", "d10", "d12"}).toString());
-        assertEquals("d4+d6+4d8+d10+d12", new DiceSet(new String[]{"d4", "d6", "4d8", "d10", "d12"}).toString());
-        assertEquals("d4+d6+d8+4d10+d12", new DiceSet(new String[]{"d4", "d6", "d8", "4d10", "d12"}).toString());
-        assertEquals("d4+d6+d8+d10+4d12", new DiceSet(new String[]{"d4", "d6", "d8", "d10", "4d12"}).toString());
+        assertEquals("d12+d10+d8+d6+4d4", new DiceSet(new String[]{"4d4", "d6", "d8", "d10", "d12"}).toString());
+        assertEquals("d12+d10+d8+4d6+d4", new DiceSet(new String[]{"d4", "4d6", "d8", "d10", "d12"}).toString());
+        assertEquals("d12+d10+4d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "4d8", "d10", "d12"}).toString());
+        assertEquals("d12+4d10+d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "d8", "4d10", "d12"}).toString());
+        assertEquals("4d12+d10+d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "d8", "d10", "4d12"}).toString());
 
-        assertEquals("6d4+6d6+d8+d10+d12", new DiceSet(new String[]{"6d4", "6d6", "d8", "d10", "d12"}).toString());
-        assertEquals("6d4+d6+6d8+d10+d12", new DiceSet(new String[]{"6d4", "d6", "6d8", "d10", "d12"}).toString());
-        assertEquals("6d4+d6+d8+6d10+d12", new DiceSet(new String[]{"6d4", "d6", "d8", "6d10", "d12"}).toString());
-        assertEquals("6d4+d6+d8+d10+6d12", new DiceSet(new String[]{"6d4", "d6", "d8", "d10", "6d12"}).toString());
-        assertEquals("d4+3d6+3d8+d10+d12", new DiceSet(new String[]{"d4", "3d6", "3d8", "d10", "d12"}).toString());
-        assertEquals("d4+3d6+d8+3d10+d12", new DiceSet(new String[]{"d4", "3d6", "d8", "3d10", "d12"}).toString());
-        assertEquals("d4+3d6+d8+d10+3d12", new DiceSet(new String[]{"d4", "3d6", "d8", "d10", "3d12"}).toString());
-        assertEquals("d4+d6+5d8+5d10+d12", new DiceSet(new String[]{"d4", "d6", "5d8", "5d10", "d12"}).toString());
-        assertEquals("d4+d6+5d8+d10+5d12", new DiceSet(new String[]{"d4", "d6", "5d8", "d10", "5d12"}).toString());
-        assertEquals("d4+d6+d8+8d10+8d12", new DiceSet(new String[]{"d4", "d6", "d8", "8d10", "8d12"}).toString());
+        assertEquals("d12+d10+d8+6d6+6d4", new DiceSet(new String[]{"6d4", "6d6", "d8", "d10", "d12"}).toString());
+        assertEquals("d12+d10+6d8+d6+6d4", new DiceSet(new String[]{"6d4", "d6", "6d8", "d10", "d12"}).toString());
+        assertEquals("d12+6d10+d8+d6+6d4", new DiceSet(new String[]{"6d4", "d6", "d8", "6d10", "d12"}).toString());
+        assertEquals("6d12+d10+d8+d6+6d4", new DiceSet(new String[]{"6d4", "d6", "d8", "d10", "6d12"}).toString());
+        assertEquals("d12+d10+3d8+3d6+d4", new DiceSet(new String[]{"d4", "3d6", "3d8", "d10", "d12"}).toString());
+        assertEquals("d12+3d10+d8+3d6+d4", new DiceSet(new String[]{"d4", "3d6", "d8", "3d10", "d12"}).toString());
+        assertEquals("3d12+d10+d8+3d6+d4", new DiceSet(new String[]{"d4", "3d6", "d8", "d10", "3d12"}).toString());
+        assertEquals("d12+5d10+5d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "5d8", "5d10", "d12"}).toString());
+        assertEquals("5d12+d10+5d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "5d8", "d10", "5d12"}).toString());
+        assertEquals("8d12+8d10+d8+d6+d4", new DiceSet(new String[]{"d4", "d6", "d8", "8d10", "8d12"}).toString());
     }
 
     @Test
     public void toStringSameDicesSeparateTest() {
-        assertEquals("2d12+2d4+5d8", new DiceSet(new String[]{"d12", "d4", "d12", "d4", "5d8"}).toString());
+        assertEquals("2d12+5d8+2d4", new DiceSet(new String[]{"d12", "d4", "d12", "d4", "5d8"}).toString());
     }
 }
